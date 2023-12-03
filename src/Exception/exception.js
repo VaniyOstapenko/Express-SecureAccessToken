712f5e67c0e4e64936b4b2cb08a7f956faea48d2
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-12-03 13:20:22 +0300

    Express-SecureAccessToken

--- src/Exception/exception.js
+++ src/Exception/exception.js
@@ -5,13 +5,13 @@ const ExceptionType = {
     USER_PWD: 'password is not found',
     USER_NAME_IS_NAN: 'a name cannot be a number',
     USER_SURNAME_IS_NAN: 'a surname cannot be a number',
-    USER_PWD_LENGTH: 'the password must be more than 8 characters long',
+    USER_PWD_LENGTH: 'the pwd must be more than 8 characters long',
     USER_EMAIL_RGX: 'incorrect mail input',
 
     USER_SERVICE_LENGTH: 'there is such a user',
     USER_SERVICE_DATA_LENGTH: 'not created',
     USER_SERVICE_EMAIL: 'user is not found',
-    USER_SERVICE_PASSWORD_MATCH: 'password does not match'
+    USER_SERVICE_PASSWORD_MATCH: 'pwd does not match'
 }
 
 module.exports = ExceptionType;
